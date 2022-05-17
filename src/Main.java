a9348abb4397eeb999dbc9ae5ca20bbf76e7a7cf
bartek <bartek.dygasinski00@gmail.com>
2022-05-17 18:20:46 +0200

    Random weights and feed forward implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Network network = new Network(4,1,3,4);
+        double[] output = network.calculate(0.2,0.9,0.3,0.4);
+        System.out.println(Arrays.toString(output));
     }
 }
--- src/Network.java
+++ src/Network.java
@@ -1,9 +1,15 @@
+import java.util.Random;
+
 public class Network {
     //wyjscia, wagi i bias przedstawione sa jako macierze, pierwszy wymiar mowi o
     //przynaleznosci do warstwy, 2 identyfikuje neuron w danej warstwie
     //3 wymiar w wagach identyfikuje neuron z poprzedniej warstwy
+
+    //output[indexOfLayer][indexOfNeuron]
     private double[][] output;
+    //weights[indexOfLayer][indexOfNeuron][indexOfprevNeuron]
     private double[][][] weights;
+    //bias[indexOfLayer][indexOfNeuron]
     private double[][] bias;
 
     public final int[] NETWORK_LAYER_SIZES;
@@ -22,13 +28,45 @@ public class Network {
         this.weights = new double[NETWORK_SIZE][][];
         this.bias = new double[NETWORK_SIZE][];
 
-        for (int i = 0; i < NETWORK_SIZE; i++) {
-            this.output[i] = new double[NETWORK_LAYER_SIZES[i]];
-            this.bias[i] = new double[NETWORK_LAYER_SIZES[i]];
+        for (int layer = 0; layer < NETWORK_SIZE; layer++) {
+            this.output[layer] = new double[NETWORK_LAYER_SIZES[layer]];
+            this.bias[layer] = new double[NETWORK_LAYER_SIZES[layer]];
+            for (int neuron = 0; neuron < NETWORK_LAYER_SIZES[layer]; neuron++) {
+                this.bias[layer][neuron] = 1;
+            }
             //wagi nie obejmuja 0 warstwy wejsciowej
-            if(i > 0) {
-                weights[i] = new double[NETWORK_LAYER_SIZES[i]][NETWORK_LAYER_SIZES[i-1]];
+            if(layer > 0) {
+                Random random = new Random();
+                weights[layer] = new double[NETWORK_LAYER_SIZES[layer]][];
+                for (int neuron = 0; neuron < NETWORK_LAYER_SIZES[layer]; neuron++) {
+                    weights[layer][neuron] = new double[NETWORK_LAYER_SIZES[layer - 1]];
+                    for (int prevLayerNeuron = 0; prevLayerNeuron < NETWORK_LAYER_SIZES[layer - 1]; prevLayerNeuron++) {
+                        weights[layer][neuron][prevLayerNeuron] = random.nextDouble(-0.5,0.5);
+                    }
+                }
             }
         }
+    }
+
+    public double sigmoid(double x) {
+        return (double) 1 / (1 + Math.exp(-x));
+    }
+
+    public double[] calculate(double... input) {
+        //inicjowanie warstwy wejsciowej
+        this.output[0] = input;
+        double sum;
+        for (int layer = 1; layer < NETWORK_SIZE; layer++) {
+            for (int neuron = 0; neuron < NETWORK_LAYER_SIZES[layer]; neuron++) {
+                sum = 0.0;
+                for (int prevLayerNeuron = 0; prevLayerNeuron < NETWORK_LAYER_SIZES[layer - 1]; prevLayerNeuron++) {
+                    //sumujemy wejscia do neuronu czyli wyjscia z warstwy poprzedniej pomnozone przez wagi pomiedzy neuronami
+                    sum += output[layer - 1][prevLayerNeuron] * weights[layer][neuron][prevLayerNeuron];
+                }
+                sum += bias[layer][neuron];
+                output[layer][neuron] = sigmoid(sum);
+            }
+        }
+        return output[NETWORK_SIZE - 1];
     }
 }
